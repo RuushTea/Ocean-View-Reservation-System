3ea3b430b7b51496c77906ab9b28edf693cb0419
RuushTea <RushdiRamz@gmail.com>
2026-02-19 17:01:52 +0530

    Add core Java class structure for reservation system

---
+++ src/main/java/com/oceanview/reservation/service/ReservationService.java
@@ -0,0 +1,4 @@
+package com.oceanview.reservation.service;
+
+public class ReservationService {
+}
---
+++ src/main/java/com/oceanview/reservation/servlet/AuthenticationServlet.java
@@ -0,0 +1,4 @@
+package com.oceanview.reservation.servlet;
+
+public class AuthenticationServlet {
+}
