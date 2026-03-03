62c9bd73a60d3a41f86ddd1b45b8c226cf68e8f0
RuushTea <RushdiRamz@gmail.com>
2026-03-04 03:11:31 +0530

    Implementing a Factory Design Pattern with UserFactory.java

---
+++ src/main/java/com/oceanview/reservation/factory/UserFactory.java
@@ -0,0 +1,13 @@
+package com.oceanview.reservation.factory;
+
+import com.oceanview.reservation.model.Admin;
+import com.oceanview.reservation.model.Guest;
+import com.oceanview.reservation.model.Staff;
+import com.oceanview.reservation.model.SystemUser;
+
+public interface UserFactory {
+    Admin createAdmin(SystemUser systemUser);
+    Staff createStaff(SystemUser systemUser);
+    Guest createGuest(Object guestData);
+    SystemUser createSystemUser(String username, String password, String fullName, boolean active);
+}
